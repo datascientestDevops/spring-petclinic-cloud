88326cefd21154ecc363e8c950f628a5d1ce7580
Dusty Milandou <dustymilandou@gmail.com>
2023-09-13 17:47:05 +0200

    feat: modify prod sh and change database url

--- kubernets/prod.sh
+++ kubernets/prod.sh
@@ -6,8 +6,6 @@ kubectl apply -f namespace.yml
 
 # init all services (ClusterIp for services and LoadBalancer for api-getway)
 kubectl apply -f services
-# Check services was created
-kubectl get svc -n spring-petclinic
 
 # launch all deployment pods
 kubectl apply -f deployments
