972fb8056ca101c577e307079694703f210a27e1
Dusty Milandou <dustymilandou@gmail.com>
2023-09-19 16:01:31 +0200

    fix all

--- kubernetes/eks_local.sh
+++ kubernetes/eks_local.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-kubectl delete -f namespace.yml --grace-period=0 --force
-
-#init a namespace
+kubectl apply -f namespace.yml#init a namespace
 kubectl apply -f namespace.yml
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
@@ -11,12 +8,11 @@ helm install vets-db-mysql bitnami/mysql --namespace spring-petclinic --version 
 helm install visits-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.10.10 --set auth.database=service_instance_db --set auth.rootPassword=datascientest_petclinic
 helm install customers-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.10.10 --set auth.database=service_instance_db --set auth.rootPassword=datascientest_petclinic
 
-
 # init all services (ClusterIp for services and LoadBalancer for api-getway)
-kubectl apply -f services
+kubectl apply -f settings
 
 # launch all deployment pods
-kubectl apply -f deployments
+kubectl apply -f apps
 
 #Check ALL
 kubectl get all -n spring-petclinic
